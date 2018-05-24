5fe6e964c4081dec2bbc11f047db95b0f2da01b7
zhang_xubin <zhangxubinhao123@163.com>
2018-05-24 15:17:53 +0800

    添加地区数据库

---
+++ app/src/main/java/com/ccwonline/coolweather/db/City.java
@@ -0,0 +1,46 @@
+package com.ccwonline.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by zhang_xubin on 2018/5/24.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private int cityCode;
+    private String cityName;
+    private int provinceId;
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+}
